e859f521588ecd95561224919562ab7d666aa043
Martine Dowden <martine@dowden.us>
2016-02-28 18:56:42 -0500

    responsive design

--- template/views/store/store.js
+++ template/views/store/store.js
@@ -9,10 +9,33 @@ angular.module('bourbon.store', ['ngRoute', 'ui.bootstrap'])
     });
   }])
 
-  .controller('storeCtrl', function ($scope, $http) {
+  .controller('storeCtrl', function ($scope, $http, $uibModal) {
   
-  $http.get('../../bourbon.json').success(function (data){
-    $scope.bottles = data;
-  });
+    $http.get('../../bourbon.json').success(function (data){
+      $scope.bottles = data;
+    });
   
-  });
+    // Modal
+  
+    $scope.open = function (size, bottle) {
+      var modalInstance = $uibModal.open({
+        animation: $scope.animationsEnabled,
+        templateUrl: 'description',
+        controller: 'modalInstanceCtrl',
+        size: size,
+        resolve: {
+          bottle: function () {
+            return bottle;
+          }
+        }
+      })
+    };
+  
+  
+  })
+
+.controller('modalInstanceCtrl', function($scope, $uibModalInstance, bottle) {
+  
+  $scope.bottle = bottle;
+  
+});
